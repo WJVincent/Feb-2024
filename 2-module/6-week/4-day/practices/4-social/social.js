2d2d08f49a8817d78f0bcf21e0518af867d9888a
William Vincent <william@william-vincent.dev>
2024-03-28 16:06:19 -0400

    4-practice

--- 2-module/6-week/4-day/practices/4-social/social.js
+++ 2-module/6-week/4-day/practices/4-social/social.js
@@ -8,27 +8,63 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here 
+		this.currentID++;
+		this.users[this.currentID] = { id: this.currentID, name };
+		this.follows[this.currentID] = new Set();
+		return this.currentID;
   }
 
   getUser(userID) {
-    // Your code here 
+	return this.users[userID] ? this.users[userID] : null;
   }
 
   follow(userID1, userID2) {
-    // Your code here 
+		if(!this.users[userID1] || !this.users[userID2]) return false;
+		this.follows[userID1].add(userID2);
+		return true;
   }
 
   getFollows(userID) {
-    // Your code here 
+		return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here 
+		const followers = new Set();
+
+		for(let id in this.follows){
+			if(this.follows[id].has(userID)){
+				followers.add(+id);
+			};
+		};
+
+		return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
-    // Your code here 
+		const q = [[userID]];
+		const v = new Set().add(userID);
+		const res = [];
+
+		while(q.length){
+			//1. get the current path
+			const currPath = q.shift();
+			const currId = currPath[currPath.length - 1];
+
+			//2. do the thing
+			if(currPath.length > degrees + 2) break;
+			if(currPath.length > 2) res.push(currId);
+
+			//3. get the neighbors
+			const neighbors = this.follows[currId];
+			for(let person of neighbors){
+				if(!v.has(person)){
+					v.add(person);
+					q.push([...currPath, person]);
+				};
+			}
+		};
+
+		return res;
   }
 }
 
